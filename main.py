4070a5dadafe44aad318053d651a4f44a97a741e
Long Nguyen <longvip1810@gmail.com>
2021-10-02 16:29:49 +0700

    add bfs check found

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ def depth_first_search(graph, node, key, visited=set(), path=[]) -> list:
         return path
 
     if node not in visited:
-        print(node)
         visited.add(node)
         for neighbor in graph[node]:
             (depth_first_search(graph, neighbor, key, visited, path))
@@ -39,6 +38,26 @@ def breath_first_travel(graph, node, visited=set(), visit_queue=[]):
         breath_first_travel(graph, visit_queue.pop(0), visited, visit_queue)
 
 
+def breath_first_search(graph, node, key, found=False, visited=set(), visit_queue=[]):
+    if node == key:
+        found = True
+        return found
+
+    # visited block
+    if node not in visited:
+        visited.add(node)
+        for neighbor in graph[node]:
+            if neighbor not in visit_queue:
+                print(neighbor)
+                visit_queue.append(neighbor)
+
+    # check all nodes are visited,
+    # should put this after "visited block" because visit_queue empty when first function call
+    if not visit_queue:
+        return False
+    return breath_first_search(graph, visit_queue.pop(0), key, found, visited, visit_queue)
+
+
 if __name__ == "__main__":
     graph = {'A': ['B', 'C'],
              'B': ['C', 'D', 'E'],
@@ -48,8 +67,5 @@ if __name__ == "__main__":
              'F': ['H'],
              'G': [],
              'H': []}
-    #depth_first_travel(graph,'A')
-    print('---')
-    breath_first_travel(graph, 'A')
 
-    #print(depth_first_search(graph, "A", "E"))
+    print(breath_first_search(graph, 'A', 'H'))
